49916f4cf10dd3096c73e5118149868cc998202f
Alyssa Blair <105877373+alyssa-blair@users.noreply.github.com>
2022-11-24 13:18:45 -0800

    Update query02.sql

--- query02.sql
+++ query02.sql
@@ -3,8 +3,13 @@
 -- not accounting for the counties that have more than 10000 population in the year of 2010, 
 -- sorted by area in descending order.
 
--- 1.05 marks: < 11 operators
--- 1.0 marks: < 13 operators
--- 0.8 marks: correct answer
-
--- select id, abbr, sum(sq_km)/10 as "area" from state join county on state.id = county.state join countypopulation on county.fips = countypopulation.county where not county in (Select county from countypopulation where year = 2010 and population > 10000) group by abbr order by sum(sq_km)/10 desc;
+select id, abbr, sum(sq_km) as "area" from state 
+  join county 
+    on (state.id = county.state) 
+  join countypopulation 
+    on (county.fips = countypopulation.county)
+where not county in 
+    (Select county from countypopulation 
+     where year = 2010 and population > 10000) 
+group by abbr 
+order by sum(sq_km) desc;
